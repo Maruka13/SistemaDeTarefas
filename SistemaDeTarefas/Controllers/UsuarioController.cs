20c3d1ee04860f873770761b524e65e05fc805e0
Maruka <emanuelle_carollina@hotmail.com>
2024-12-05 22:22:31 -0300

    get post put delete

--- SistemaDeTarefas/Controllers/UsuarioController.cs
+++ SistemaDeTarefas/Controllers/UsuarioController.cs
@@ -23,5 +23,38 @@ namespace SistemaDeTarefas.Controllers
             List<UsuarioModel> usuarios = await _usuarioRepositorio.BuscarTodosUsuarios();
             return Ok();
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<UsuarioModel>>> BuscarPorId(int id)
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.BuscarPorId(id);
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UsuarioModel>> Cadastrard([FromBody] UsuarioModel usuarioModel)
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.Adicionar(usuarioModel);
+            return Ok(usuario);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UsuarioModel>> Atualizar([FromBody] UsuarioModel usuarioModel, int id)
+        {
+            usuarioModel.Id = id;
+            UsuarioModel usuario = await _usuarioRepositorio.Atualizar(usuarioModel, id);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UsuarioModel>> Apagar(int id)
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.Apagar(id);
+            return Ok(apagado);
+        }
+
+
+
+
     }
 }
